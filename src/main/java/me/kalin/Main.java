5bd8833a45df83c5897601db8340bbf56c5a270e
hahava <hahava5696@gmail.com>
2021-11-29 23:37:03 +0900

    init

---
+++ src/main/java/me/kalin/Main.java
@@ -0,0 +1,51 @@
+package me.kalin;
+
+import me.kalin.entity.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("my-jpa");
+
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        transaction.begin();
+
+
+        try {
+            // insert
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("haha");
+            entityManager.persist(member);
+
+            // select
+            Member firstMember = entityManager.find(Member.class, 1L);
+            System.out.println(firstMember);
+
+            System.out.println(firstMember.equals(member));
+            System.out.println(firstMember == member);
+
+            // update
+            member.setName("hahava");
+
+            // delete
+            entityManager.remove(firstMember);
+
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            transaction.rollback();
+        }
+
+        entityManager.close();
+
+        entityManagerFactory.close();
+    }
+}
